8b05ebc1d7d7e3e1362ed1bf4f6258ec01288d9e
vipin pandey <vipinpandey@vipins-MacBook-Air.local>
2024-06-02 17:29:11 +0530

    added avatar

--- client/src/component/user/index.js
+++ client/src/component/user/index.js
@@ -1,100 +1,93 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React from 'react';
-import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import { Card, Descriptions, Avatar } from 'antd';
 import PropTypes from 'prop-types';
-import constant_helper from "../../helper/constant_helper";
-import collection_helper from "../../helper/collection_helper";
-import { Card, Typography } from 'antd';
-import {  Row, Col } from 'antd';
-import * as antd from 'antd'
+import constant_helper from '../../helper/constant_helper';
+import collection_helper from '../../helper/collection_helper';
+import { UserOutlined } from '@ant-design/icons';
+import maleAvatar from '../../assets/boy-avatar.png'; // Add the path to your male avatar image
+import femaleAvatar from '../../assets/girl-avatar.png'; // Add the path to your female avatar image
 
 const attributes = [
-    {
-        "id": "user_email",
-        "name": "User Email"
-    },
-    {
-        "id": "user_firstname",
-        "name": "First Name"
-    },
-    {
-        "id": "user_laststname",
-        "name": "Is Class Teacher"
-    },
-    {
-        "id": "user_id",
-        "name": "User Id"
-    },
-    {
-        "id": "user_gender",
-        "name": "Standard"
-    }
-]
+  {
+    id: 'user_email',
+    name: 'User Email',
+  },
+  {
+    id: 'user_firstname',
+    name: 'First Name',
+  },
+  {
+    id: 'user_lastname',
+    name: 'Is Class Teacher',
+  },
+  {
+    id: 'user_id',
+    name: 'User Id',
+  },
+  {
+    id: 'user_gender',
+    name: 'Standard',
+  },
+];
 
 class UserComponent extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			loading: false
-		};
-		this.set_state = this.set_state.bind(this);
-        this.render_description = this.render_description.bind(this);
-	}
+    this.state = {
+      loading: false,
+    };
+    this.set_state = this.set_state.bind(this);
+    this.render_description = this.render_description.bind(this);
+  }
 
-	componentDidMount() {
-	}
+  componentDidMount() {}
 
-	// updating
-	// eslint-disable-next-line no-unused-vars
-	shouldComponentUpdate(nextProps, nextState) {
-		return true;
-	}
+  // updating
+  // eslint-disable-next-line no-unused-vars
+  shouldComponentUpdate(nextProps, nextState) {
+    return true;
+  }
 
-	// unmount
-	componentWillUnmount() {
+  // unmount
+  componentWillUnmount() {}
 
-	}
+  render_description() {
+    let data_source = collection_helper.process_get_all_items();
+    const avatarSrc = data_source.GENDER === 'Male' ? maleAvatar : data_source.GENDER === 'Female' ? femaleAvatar : "";
 
-    render_description() {
-        
-        let data_source = collection_helper.process_get_all_items();
-        return (
-          <antd.Card>
-            <antd.Descriptions title="User Info">
-                <antd.Descriptions.Item label="User Id">{data_source.user_id}</antd.Descriptions.Item>
-                <antd.Descriptions.Item label="First Name">{data_source.user_firstname}</antd.Descriptions.Item>
-                <antd.Descriptions.Item label="Last Name">{data_source.user_lastname}</antd.Descriptions.Item>
-                <antd.Descriptions.Item label="Email">{data_source.user_email}</antd.Descriptions.Item>
-                <antd.Descriptions.Item label="Gender">
-                {data_source.GENDER}
-                </antd.Descriptions.Item>
-            </antd.Descriptions>
-          </antd.Card>
-        )
-      }
+    return (
+      <Card>
+        <Descriptions title="User Info">
+          <Descriptions.Item label="User Avatar">
+            <Avatar size={64} src={avatarSrc} icon={<UserOutlined />} />
+          </Descriptions.Item>
+          <Descriptions.Item label="User Id">{data_source.user_id}</Descriptions.Item>
+          <Descriptions.Item label="First Name">{data_source.user_firstname}</Descriptions.Item>
+          <Descriptions.Item label="Last Name">{data_source.user_lastname}</Descriptions.Item>
+          <Descriptions.Item label="Email">{data_source.user_email}</Descriptions.Item>
+          <Descriptions.Item label="Gender">{data_source.GENDER}</Descriptions.Item>
+        </Descriptions>
+      </Card>
+    );
+  }
 
-	
-	set_state(key, value) {
-		this.setState({
-			...this.state,
-			[key]: value
-		})
-	}
+  set_state(key, value) {
+    this.setState({
+      ...this.state,
+      [key]: value,
+    });
+  }
 
-	render() {
-		return (
-            <>
-                {this.render_description()}
-            </>
-        )
-	}
+  render() {
+    return <>{this.render_description()}</>;
+  }
 }
 
 UserComponent.propTypes = {
-	// class: PropTypes.object.isRequired,
-	app_action: PropTypes.object.isRequired,
+  // class: PropTypes.object.isRequired,
+  app_action: PropTypes.object.isRequired,
 };
 
-
-export default UserComponent
+export default UserComponent;
